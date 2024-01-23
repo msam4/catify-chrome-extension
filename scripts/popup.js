a341c65b383d0c7ce4c34ec90909eeb599dbe85d
Michael Sam <sam.michael.h@gmail.com>
2024-01-23 17:33:28 +0900

    Reword the comments in the files

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,11 +1,11 @@
 function sendCatifyMsg() {
-  // TODO: Write a function to send a message to the active tab to 'catify' it
+  // TODO: Write a function to send a message to the active tab to activate the extensio
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) { // Finds tabs that are active in the current window
     chrome.tabs.sendMessage(tabs[0].id, { action: 'catify' }); // Sends a message (object) to the first tab (tabs[0])
   });
 }
 
-// TODO: Add an event listener to trigger the function above when clicking the 'catify' button
+// Add an event listener to trigger the function above when clicking the button
 const catifyBtn = document.querySelector("#catify")
 catifyBtn.addEventListener('click', (event) => {
   sendCatifyMsg()
